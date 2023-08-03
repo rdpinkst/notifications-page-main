953b56534b190f27ee71818ad9a15eb9043d002d
Ryan P <rdpinkstaff@gmail.com>
2023-08-02 19:37:37 -0600

    Working on figuring out how to remove event listener after click

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const getAllArticles = document.querySelectorAll(".ind-notification");
+const countDOM = document.querySelector(".not-count");
+let count = 0;
+let allNodes = [];
+let unreadNodes = []
 
-getAllArticles.forEach(notification => {
-    const active = notification.querySelector(".unread");
-    if(active) {
+getAllArticles.forEach((notification, index) => {
+    const unread = notification.querySelector(".unread");
+    allNodes.push(notification);
+    unreadNodes.push(unread);
+    if(unread) {
+        count += 1;
         notification.classList.add("change-background");
-        notification.addEventListener("click", event => {
-            event.currentTarget.classList.remove("change-background");
-        })
-    }
+        notification.addEventListener("click", clearNotification);
+        countDOM.innerHTML = `<span>${count}</span>`;
+
+    } 
 })
 
-function clearNotification() {
-
+console.log(allNodes)
+function clearNotification(event) {
+    event.currentTarget.classList.remove("change-background");
+    const dot = event.currentTarget.querySelector(".one-line .description div");
+    count -= 1;
+    dot.classList.remove("unread");
+    dot.classList.add("read");
+    countDOM.innerHTML = `<span>${count}</span>`;
 }
+
+function removeListener(number) {
+    number.forEach(removed => {
+        removed.removeEventListener("click", clearNotification)
+    })
+}
+
