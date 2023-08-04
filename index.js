e21fb903299593dd41ebc684800eb519594fc705
Ryan P <rdpinkstaff@gmail.com>
2023-08-03 18:26:59 -0600

    Figured out event listeners and need to refactor

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const getAllArticles = document.querySelectorAll(".ind-notification");
 const countDOM = document.querySelector(".not-count");
+const clearAll = document.querySelector("#all-read");
 let count = 0;
 let allNodes = [];
 let unreadNodes = []
 
 getAllArticles.forEach((notification, index) => {
     const unread = notification.querySelector(".unread");
-    allNodes.push(notification);
-    unreadNodes.push(unread);
+    if(unread) {
+      unreadNodes.push(notification);  
+    }
+    
     if(unread) {
         count += 1;
         notification.classList.add("change-background");
-        notification.addEventListener("click", clearNotification);
+        notification.addEventListener("click", clearNotification, {once: true});
         countDOM.innerHTML = `<span>${count}</span>`;
 
     } 
 })
 
-console.log(allNodes)
+console.log(unreadNodes)
 function clearNotification(event) {
     event.currentTarget.classList.remove("change-background");
     const dot = event.currentTarget.querySelector(".one-line .description div");
@@ -27,9 +30,21 @@ function clearNotification(event) {
     countDOM.innerHTML = `<span>${count}</span>`;
 }
 
-function removeListener(number) {
-    number.forEach(removed => {
-        removed.removeEventListener("click", clearNotification)
-    })
-}
+clearAll.addEventListener("click", event => {
+    if(unreadNodes.length > 0) {
+        unreadNodes.forEach(node => {
+            node.classList.remove("change-background");
+            console.log(node)
+            const dot = node.querySelector(".one-line .description div");
+            count = 0;
+            dot.classList.remove("unread");
+            dot.classList.add("read");
+            countDOM.innerHTML = `<span>${count}</span>`;
+            node.removeEventListener("click", clearNotification);
+        });
+        unreadNodes = [];
+    }
+})
+
+
 
